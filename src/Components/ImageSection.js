ea0b479f1a116a75270b83cecc837634ece8dfe5
Vruditdp <vruditdpatel1809@gmail.com>
2021-07-21 17:08:20 +0530

    vrudit.pdf added

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -38,7 +38,7 @@ function ImageSection() {
                     
                     <button className="btn">Download Cv</button>
                 </a> */}
-                <a href="abc.pdf" download className="res">
+                <a href="vrudit.pdf" download className="res">
                     <button className="btn">Download Cv</button>
                 </a>
             </div>
